af577a33b91fca44e14c1542cedc0afefe34dd30
Flaviano Honorato <flaviano.honorato@gmail.com>
2017-03-11 11:08:37 -0300

    added users in router service provider

--- src/Service/RouterServiceProvider.php
+++ src/Service/RouterServiceProvider.php
@@ -7,10 +7,20 @@ use Pimple\ServiceProviderInterface;
 
 class RouterServiceProvider implements ServiceProviderInterface
 {
-  public function register(Container $app)
-  {
-    $app->get($app['api_version'] . '/beers', 'beers:index');
-    $app->get($app['api_version'] . '/beers/{id}', 'beers:getBeers');
+    public function register(Container $app)
+    {
+        $beers_url = '/beers';
+        $app->get($app['api_version'] . $beers_url, 'beers:get');
+        $app->get($app['api_version'] . $beers_url . '{id}', 'beers:get');
+        $app->post($app['api_version'] . $beers_url, 'beers:create');
+        $app->put($app['api_version'] . $beers_url, 'beers:update');
+        $app->delete($app['api_version'] . $beers_url . '{id}', 'beers:delete');
 
-  }
+        $users_url = '/users';
+        $app->get($app['api_version'] . $users_url, 'users:get');
+        $app->get($app['api_version'] . $users_url . '{id}', 'users:get');
+        $app->post($app['api_version'] . $users_url, 'users:create');
+        $app->put($app['api_version'] . $users_url, 'users:update');
+        $app->delete($app['api_version'] . $users_url . '{id}', 'users:delete');
+    }
 }
