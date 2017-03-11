d9647766c70dfe456f0e5a9db07e669e77a605dd
Flaviano Honorato <flaviano.honorato@gmail.com>
2017-03-11 11:08:04 -0300

    refactored controllerServiceProvider

--- src/Service/ControllerServiceProvider.php
+++ src/Service/ControllerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace ApiMaster\Service;
 
 use ApiMaster\Controller\BeerController;
+use ApiMaster\Controller\UserController;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
@@ -10,8 +11,12 @@ class ControllerServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
-      $app['beers'] = function(Container $app){
-        return new BeerController($app);
-      };
+        $app['beers'] = function(Container $app){
+            return new BeerController($app);
+        };
+
+        $app['users'] = function(Container $app){
+            return new UserController($app);
+        };
     }
 }
