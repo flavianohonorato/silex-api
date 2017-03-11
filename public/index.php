3a58d099125f451ee29224ef463ccfef8667bc5e
Flaviano Honorato <flaviano.honorato@gmail.com>
2017-03-11 09:30:48 -0300

    atualização de configs, reestruturação de pastas, add dependências, etc

--- public/index.php
+++ public/index.php
@@ -2,27 +2,6 @@
 
 chdir(dirname(__DIR__));
 
-require 'vendor/autoload.php';
-
-use ApiMaster\Service\ControllerServiceProvider;
-use ApiMaster\Service\RouterServiceProvider;
-use Silex\Application;
-
-$app = new Application();
-
-
-// $app->get('/', function(Application $app) {
-//   return $app->escape("OK");
-// });
-
-$app->register(new Silex\Provider\ServiceControllerServiceProvider());
-
-$app->register(new RouterServiceProvider());
-$app->register(new ControllerServiceProvider());
-
-
-
-
-
+require "app.php";
 
 $app->run();
